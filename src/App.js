9dbfae43fdf3f30b7b5cf17023161dcd28637ac9
lee <wnsaud6016@naver.com>
2018-11-16 13:48:49 +0900

    componentDidMount & BoundaryHoc

--- src/App.js
+++ src/App.js
@@ -1,48 +1,73 @@
-import React, { Component ,Fragment } from 'react';
-import {createPortal} from 'react-dom';
+import React, { Component, Fragment } from "react";
+import { createPortal } from "react-dom";
 
+const BoundaryHOC = ProtectedComponent =>
+  class Boundary extends Component {
+    state = {
+      hasError: false
+    };
+    componentDidCatch = () => {
+      this.setState({
+        hasError: true
+      });
+    };
+    render() {
+      const { hasError } = this.state;
+      if (hasError) {
+        return <ErrorFallback />;
+      } else {
+        return <ProtectedComponent />;
+      }
+    }
+  };
 
+class ErrorMaker extends Component {
+  state = {
+    friends: ["jisu", "flynn", "daal", "kneeprayer"]
+  };
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.setState({
+        friends: undefined
+      });
+    }, 2000);
+  };
+  render() {
+    const { friends } = this.state;
+    return friends.map(friend => ` ${friend} `);
+  }
+}
 
+const PErrorMaker = BoundaryHOC(ErrorMaker);
 
+class Portals extends Component {
+  render() {
+    return createPortal(<Message />, document.getElementById("touchme"));
+  }
+}
 
+const PPortals = BoundaryHOC(Portals);
+
+const Message = () => "Just touched it!";
+
+class ReturnTypes extends Component {
+  render() {
+    return "hello";
+  }
+}
+
+const ErrorFallback = () => "Sorry something went wrong";
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <ReturnTypes/>
-        <ReturnTypes/>
-        <ReturnTypes/>
-        <Portals></Portals>
+        <ReturnTypes />
+        <PPortals />
+        <PErrorMaker />
       </Fragment>
-
     );
   }
 }
 
-class ReturnTypes extends Component{
-  render(){
-    return(
-      <Fragment>
-      
-      <div>hello</div>
-      
-      </ Fragment>
-    )
-  }
-
-}
-
-
-class Portals extends Component{
-  render(){
-    return createPortal(
-      <Message/>,
-      document.querySelector('#touchme')
-    );
-  }
-}
-
-const Message =()=> "Just touched it";
-
-export default App;
+export default BoundaryHOC(App);
